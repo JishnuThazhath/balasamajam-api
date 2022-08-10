f30340f9ee904541351f93cd029e01055e5a983f
jishnu <jishnuanchery@gmail.com>
2022-08-11 00:01:22 +0530

    Jishnu | First commit

---
+++ src/main/java/com/balasamajam/BalasamajamApplication.java
@@ -0,0 +1,13 @@
+package com.balasamajam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalasamajamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalasamajamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balasamajam/BalasamajamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balasamajam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalasamajamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
